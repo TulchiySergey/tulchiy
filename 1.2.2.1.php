afda94feb936ea538de27b350a2a71618631caab
Tulchiy <test@gmail.com>
2018-02-08 14:24:36 +0200

    111

--- 1.2.2.1.php
+++ 1.2.2.1.php
@@ -1,4 +1,26 @@
 <?php
-$a = array(1, 2, array("a", "b", "c"));
-var_dump($a);
+$x1 = 'this is string';
+$x2 = 0.123;
+$x3 = true;
+var_dump($x1, $x2, $x3);
 ?>
+
+<br><br>
+
+<?php
+$x1 = 'this is string';
+print_r($x1);
+?>
+
+
+    <br><br>
+
+<?php
+$x2 = 0.123;
+var_export($x2);
+?>
+
+
+
+
+
