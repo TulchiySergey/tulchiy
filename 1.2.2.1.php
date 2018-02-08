d3bad94578cdc7966105cfd7b6d1d7db3d8b6733
Tulchiy <test@gmail.com>
2018-02-08 17:08:27 +0200

    111

--- 1.2.2.1.php
+++ 1.2.2.1.php
@@ -1,5 +1,24 @@
 <?php
 $x1 = 'this is string';
+echo '$x1 is: ';
+var_dump($x1);
+
+
+$x2 = 0.123;
+echo '$x2 is: ';
+var_dump($x2);
+
+
+$x3 = true;
+echo '$x3 is: ';
+var_dump($x3);
+?>
+
+
+<br><br>
+
+<?php
+$x1 = 'this is string';
 $x2 = 0.123;
 $x3 = true;
 var_dump($x1, $x2, $x3);
@@ -9,6 +28,7 @@ var_dump($x1, $x2, $x3);
 
 <?php
 $x1 = 'this is string';
+echo '$x1 is: ';
 print_r($x1);
 ?>
 
@@ -17,9 +37,19 @@ print_r($x1);
 
 <?php
 $x2 = 0.123;
+echo '$x2 is: ';
 var_export($x2);
 ?>
 
+
+<br><br>
+
+
+<?php
+$x3 = true;
+echo '$x3 is: ';
+var_export($x3);
+?>
 
 
 
